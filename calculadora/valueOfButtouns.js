e1facf3464236327fd7b5dbfa294eb987767bb25
daniszcode <danirbd13@hotmail.com>
2022-03-09 11:59:53 -0300

    iniciando script da calculadora

---
+++ calculadora/valueOfButtouns.js
@@ -0,0 +1,41 @@
+const results = document.querySelector(".results");
+
+const numberOne = () => {
+  results.textContent = 1;
+};
+
+const numberTwo = () => {
+  results.textContent = 2;
+};
+
+const numberThree = () => {
+  results.textContent = 3;
+};
+
+const numberFour = () => {
+  results.textContent = 4;
+};
+
+const numberFive = () => {
+  results.textContent = 5;
+};
+
+const numberSix = () => {
+  results.textContent = 6;
+};
+
+const numberSeven = () => {
+  results.textContent = 7;
+};
+
+const numberEight = () => {
+  results.textContent = 8;
+};
+
+const numberNine = () => {
+  results.textContent = 9;
+};
+
+const numberZero = () => {
+  results.textContent = 0;
+};
