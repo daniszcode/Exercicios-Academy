b13bd541087d08c4fa38b152960fa8e07711801a
daniszcode <danirbd13@hotmail.com>
2022-03-10 11:13:00 -0300

    criando arquivo valueOfButtouns para capturar valores dos botoes e mostrar no display

--- calculadora/valueOfButtouns.js
+++ calculadora/valueOfButtouns.js
@@ -1,41 +1,41 @@
+export const allButtouns = document.querySelectorAll("#allButtouns");
 const results = document.querySelector(".results");
 
-const numberOne = () => {
-  results.textContent = 1;
-};
-
-const numberTwo = () => {
-  results.textContent = 2;
-};
-
-const numberThree = () => {
-  results.textContent = 3;
-};
-
-const numberFour = () => {
-  results.textContent = 4;
-};
-
-const numberFive = () => {
-  results.textContent = 5;
-};
-
-const numberSix = () => {
-  results.textContent = 6;
-};
-
-const numberSeven = () => {
-  results.textContent = 7;
-};
-
-const numberEight = () => {
-  results.textContent = 8;
-};
-
-const numberNine = () => {
-  results.textContent = 9;
-};
-
-const numberZero = () => {
-  results.textContent = 0;
+export const valueOfButtouns = () => {
+  allButtouns.forEach((buttoun) => {
+    buttoun.addEventListener("click", function () {
+      switch (buttoun.textContent) {
+        case "1":
+          results.textContent = 1;
+          break;
+        case "2":
+          results.textContent = 2;
+          break;
+        case "3":
+          results.textContent = 3;
+          break;
+        case "4":
+          results.textContent = 4;
+          break;
+        case "5":
+          results.textContent = 5;
+          break;
+        case "6":
+          results.textContent = 6;
+          break;
+        case "7":
+          results.textContent = 7;
+          break;
+        case "8":
+          results.textContent = 8;
+          break;
+        case "9":
+          results.textContent = 9;
+          break;
+        case "0":
+          results.textContent = 0;
+          break;
+      }
+    });
+  });
 };
